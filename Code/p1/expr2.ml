79fc4e37f498f27f812128fec063384a47d6c285
Daniel Rodriguez Sanchez <daniel.rodriguez.sanchez1@udc.es>
2021-11-22 10:09:20 +0100

    Practica 1

---
+++ Code/p1/expr2.ml
@@ -0,0 +1,9 @@
+let u = 2 + 3 - 1 * 3 /5;;
+
+let v = asin 1.0;;
+
+let w =  char_of_int 68;;
+
+let x = 3 < 5 + 4;;
+
+let y = if 4 < 5 then "Menor" else "Mayor o igual";;
---
+++ Code/p1/nombre.ml
@@ -0,0 +1,3 @@
+print_string "Daniel Rodriguez Sanchez\n";;
+
+print_string "daniel.rodriguez.sanchez1@udc.es\n";;
